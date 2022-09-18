4ecf2e7893ab2f53fd2849ce81ca37f39c101779
marina <litvinova_06@bk.ru>
2022-09-18 23:12:47 +0500

    Задача

---
+++ Work_string/Program.cs
@@ -0,0 +1,35 @@
+﻿// Написать программу, которая из имеющегося массива строк формирует массив из строк, длина которых меньше или равна 3 символа.
+// Первоначальный массив можно ввсести с клавиатуры, либо задать а старте выполнения алгоритма.
+// При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
+// Примеры:
+//["hello", "2", ":-)"] -> ["2", ":-)"]
+// ["1234", "1567", "-2"] -> ["-2"]
+// ["Russia", "Denmark", "Kazan"] -> []
+
+class Program
+{
+    static void Main (string []args)
+    {
+        string[]work;
+        
+        Console.WriteLine("Введите слова, символы, числа(через пробел):");
+        string array = Console.ReadLine();
+        work = array.Split(' ');
+        var result = new string [work.Length];
+        var newSize = 0;
+        var go = work;
+        foreach (var item in work)
+        {
+            if (item.Length <=3)
+            {
+                result[newSize] = item;
+                newSize++;
+            }
+        }   
+            Console.Write(string.Join('\n', result, 0, newSize ));
+          Console.ReadKey();
+
+        
+    }
+}
+
